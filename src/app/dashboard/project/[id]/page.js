3518d11eeaf60ed5c231656cd7f49ce6a0ae93dc
l4legenda <l4legenda@gmail.com>
2025-03-13 15:13:59 +0500

    feat: priority

--- src/app/dashboard/project/[id]/page.js
+++ src/app/dashboard/project/[id]/page.js
@@ -14,13 +14,22 @@ export default function ProjectView({ params }) {
 
     const [isModalCreateTask, setModalCreateTask] = useState(false)
 
-    const [listHighPriorityTask, setListHighPriorityTask] = useState([])
+    const [listPriorityTask, setListPriorityTask] = useState([])
 
+    const listHighPriority = listPriorityTask.filter((v, i) => (
+        v?.priority === 2
+    ))
+    const listMiddlePriority = listPriorityTask.filter((v, i) => (
+        v?.priority === 1
+    ))
+    const listLowPriority = listPriorityTask.filter((v, i) => (
+        v?.priority === 0
+    ))
 
     const handleListTask = async () => {
         const response = await fetchGetListTask(id_project, search);
-        if (response.length != listHighPriorityTask.length) {
-            setListHighPriorityTask(response);
+        if (response.length != listPriorityTask.length) {
+            setListPriorityTask(response);
         }
 
     }
@@ -50,44 +59,60 @@ export default function ProjectView({ params }) {
                 />
             </div>
             <div className={styles.container_tasks}>
-                <div className={styles.priority}>
-                    <FontAwesomeIcon icon={faFire} className={styles.priority_icon} />
-                    <div className={styles.priority_text}>Высокий приоритет</div>
-                </div>
-                <div className={styles.task_box}>
-                    <div className={styles.task_box_border}></div>
-                    <div className={styles.task_box_content}>
-                        {listHighPriorityTask.map((v, i) => (
-                            v?.priority === 2 ? <Task key={i} data={v} /> : null
-                        ))}
+                {listHighPriority.length > 0 && (
+                    <div>
+                        <div className={styles.priority}>
+                            <FontAwesomeIcon icon={faFire} className={styles.priority_icon} />
+                            <div className={styles.priority_text}>Высокий приоритет</div>
+                        </div>
 
-                    </div>
-                </div>
-                <div className={styles.priority}>
-                    <FontAwesomeIcon icon={faPersonWalking} className={styles.priority_icon} />
-                    <div className={styles.priority_text}>Средний приоритет</div>
-                </div>
-                <div className={styles.task_box}>
-                    <div className={styles.task_box_border}></div>
-                    <div className={styles.task_box_content}>
-                        {listHighPriorityTask.map((v, i) => (
-                            v?.priority === 1 ? <Task key={i} data={v} /> : null
-                        ))}
-                    </div>
-                </div>
+                        <div className={styles.task_box}>
+                            <div className={styles.task_box_border}></div>
+                            <div className={styles.task_box_content}>
+                                {listHighPriority.map((v, i) => (
+                                    <Task key={i} data={v} />
+                                ))}
 
-                <div className={styles.priority}>
-                    <FontAwesomeIcon icon={faPersonHiking} className={styles.priority_icon} />
-                    <div className={styles.priority_text}>Низкий приоритет</div>
-                </div>
-                <div className={styles.task_box}>
-                    <div className={styles.task_box_border}></div>
-                    <div className={styles.task_box_content}>
-                        {listHighPriorityTask.map((v, i) => (
-                            v?.priority === 0 ? <Task key={i} data={v} /> : null
-                        ))}
+                            </div>
+                        </div>
                     </div>
-                </div>
+
+                )}
+                {listMiddlePriority.length > 0 && (
+                    <div>
+                        <div className={styles.priority}>
+                            <FontAwesomeIcon icon={faPersonWalking} className={styles.priority_icon} />
+                            <div className={styles.priority_text}>Средний приоритет</div>
+                        </div>
+                        <div className={styles.task_box}>
+                            <div className={styles.task_box_border}></div>
+                            <div className={styles.task_box_content}>
+                                {listMiddlePriority.map((v, i) => (
+                                    <Task key={i} data={v} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                )}
+
+                {listLowPriority.length > 0 && (
+                    <div>
+                        <div className={styles.priority}>
+                            <FontAwesomeIcon icon={faPersonHiking} className={styles.priority_icon} />
+                            <div className={styles.priority_text}>Низкий приоритет</div>
+                        </div>
+                        <div className={styles.task_box}>
+                            <div className={styles.task_box_border}></div>
+                            <div className={styles.task_box_content}>
+                                {listLowPriority.map((v, i) => (
+                                    <Task key={i} data={v} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                )}
+
             </div>
 
         </div>
